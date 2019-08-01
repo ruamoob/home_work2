6b41319618dfcc9ff28ec75d1105cc9ed20e3b14
ruamoob <olmolm@yandex.ru>
2019-08-02 03:47:59 +0600

    Create_Init

---
+++ Warehouse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchy
+{
+  public class Warehouse
+    {
+        String nameWarehouse;
+        String adress;
+        String city;
+        String area;
+        int postCode;
+
+        public Warehouse(string nameWarehouse, string adress, string city, string area, int postCode)
+        {
+            this.nameWarehouse = nameWarehouse ?? throw new ArgumentNullException(nameof(nameWarehouse));
+            this.adress = adress ?? throw new ArgumentNullException(nameof(adress));
+            this.city = city ?? throw new ArgumentNullException(nameof(city));
+            this.area = area ?? throw new ArgumentNullException(nameof(area));
+            this.postCode = postCode;
+        }
+    }
+}
